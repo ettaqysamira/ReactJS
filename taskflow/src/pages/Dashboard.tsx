c1669873d60f71f7f704f1ab20f37b55d072ef18
ettaqysamira <ettaqy.samira.20@gmail.com>
2026-04-21 01:00:50 +0100

    TP4

--- taskflow/src/pages/Dashboard.tsx
+++ taskflow/src/pages/Dashboard.tsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
 import { useAuth } from '../features/auth/AuthContext';
 import api from '../api/axios';
-import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 import MainContent from '../components/MainContent';
 import styles from './Dashboard.module.css';
 import ProjectForm from '../components/ProjectForm';
 import axios from 'axios';
+import HeaderMUI from '../components/HeaderMUI';
 
 interface Project { id: string; name: string; color: string; }
 interface Column { id: string; title: string; tasks: string[]; }
@@ -119,7 +119,7 @@ const [saving, setSaving] = useState(false);
 
   return (
     <div className={styles.layout}>
-      <Header
+      <HeaderMUI
         title="TaskFlow"
         onMenuClick={() => setSidebarOpen(p => !p)}
         userName={authState.user?.name}
